4c7ce5208c57f9c7d567adca72a5af141bc2d810
Benjamin <1234hteimaj4321@gmail.com>
2020-12-18 14:28:15 -0700

    added all funcs for division mult minus etc

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -61,6 +61,24 @@ export const Calculator = () => {
       const unstringed2: number = parseInt(currentNum2);
       const newTotal = doshit.add(unstringed1, unstringed2);
       setTotal(newTotal);
+    } else if (currentOperator === "-" && currentNum2 !== null) {
+      console.log(currentNum2);
+      const unstringed1: number = parseInt(currentNum1);
+      const unstringed2: number = parseInt(currentNum2);
+      const newTotal = doshit.subtract(unstringed1, unstringed2);
+      setTotal(newTotal);
+    } else if (currentOperator === "%" && currentNum2 !== null) {
+      console.log(currentNum2);
+      const unstringed1: number = parseInt(currentNum1);
+      const unstringed2: number = parseInt(currentNum2);
+      const newTotal = doshit.divide(unstringed1, unstringed2);
+      setTotal(newTotal);
+    } else if (currentOperator === "*" && currentNum2 !== null) {
+      console.log(currentNum2);
+      const unstringed1: number = parseInt(currentNum1);
+      const unstringed2: number = parseInt(currentNum2);
+      const newTotal = doshit.multiply(unstringed1, unstringed2);
+      setTotal(newTotal);
     }
   };
   return (
