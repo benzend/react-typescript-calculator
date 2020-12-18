44e59ca728d8a23bc21b36e42d011f50c66a4470
Benjamin <1234hteimaj4321@gmail.com>
2020-12-17 19:28:21 -0700

    added calculator basic stying

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -1,13 +1,24 @@
+import { makeStyles, Box } from "@material-ui/core";
+
 import { Display } from "./display/Display";
 import { Operator } from "./operator/Operator";
 import { Number } from "./number/Number";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    height: "500px",
+    width: "500px",
+    background: "lightBlue",
+  },
+}));
 
 // We're going to store all state and data in here
 export const Calculator = () => {
   const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   const ops = ["+", "-", "%", "*"];
+  const classes = useStyles();
   return (
-    <>
+    <Box className={classes.root}>
       <Display />
       {nums.map((num: any) => (
         <Number num={num} />
@@ -15,6 +26,6 @@ export const Calculator = () => {
       {ops.map((op: string) => (
         <Operator op={op} />
       ))}
-    </>
+    </Box>
   );
 };
