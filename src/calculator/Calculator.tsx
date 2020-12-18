cff85d2b6714e47d8adca77b92018258cabfc076
Benjamin <1234hteimaj4321@gmail.com>
2020-12-17 19:14:34 -0700

    added data for calculator + Operator components

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -7,7 +7,7 @@ export const Calculator = () => {
     <>
       <Display />
       <Number nums={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]} />
-      <Operator />
+      <Operator ops={["+", "-", "%", "*"]} />
     </>
   );
 };
--- src/calculator/operator/Operator.tsx
+++ src/calculator/operator/Operator.tsx
@@ -1,7 +1,10 @@
-export const Operator = () => {
+export const Operator = ({ ops }: any) => {
   return (
     <>
       <h1>Operator</h1>
+      {ops.map((op: string) => (
+        <div>{op}</div>
+      ))}
     </>
   );
 };
