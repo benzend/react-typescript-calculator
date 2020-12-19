8afdf6db1a0b205ee20f5552a66f1e024c1e0212
Benjamin <1234hteimaj4321@gmail.com>
2020-12-19 12:51:31 -0700

    reorganizing components for added flexability + less bug temptation

---
+++ src/calculator/components/Dot.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@material-ui/core";
+
+export const Dot = () => {
+  return <Button>.</Button>;
+};
---
+++ src/calculator/components/NegativeInt.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@material-ui/core";
+
+export const NegativeInt = () => {
+  return <Button>-</Button>;
+};
