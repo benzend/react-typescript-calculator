00294748555b1a2ac2213c80e45935fb3375d1b8
Benjamin <1234hteimaj4321@gmail.com>
2020-12-19 12:48:32 -0700

    added styling for equals

--- src/calculator/components/Equals.tsx
+++ src/calculator/components/Equals.tsx
@@ -1,11 +1,9 @@
+import { Button } from "@material-ui/core";
+
 interface Props {
   equalsHandler: any;
 }
 
 export const Equals = ({ equalsHandler }: Props) => {
-  return (
-    <div onClick={equalsHandler}>
-      <h1>=</h1>
-    </div>
-  );
+  return <Button onClick={equalsHandler}>=</Button>;
 };
