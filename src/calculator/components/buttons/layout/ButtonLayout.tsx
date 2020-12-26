5a955aedee384fc5dd08e75ee39f0057b6a15510
Benjamin <1234hteimaj4321@gmail.com>
2020-12-26 13:31:41 -0700

    complete reset

--- src/calculator/components/buttons/layout/ButtonLayout.tsx
+++
@@ -1,6 +0,0 @@
-import { Box } from "@material-ui/core";
-import { ChildrenOnlyProps } from "../../../layout/CalculatorLayout";
-
-export const ButtonLayout = ({ children }: ChildrenOnlyProps) => {
-  return <Box margin="3px">{children}</Box>;
-};
